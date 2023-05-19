14275f7cb49a351d0c43905977fc50e6d2223fd0
wandileo <wandilemaluleke@gmail.com>
2023-05-19 08:18:56 +0200

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  document.getElementById('root')
 );
 
